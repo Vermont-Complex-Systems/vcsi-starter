3d4f07a15753b1dee7597a71e3077efb994e6574
Jonathan St-Onge <jonathanstonge7@gmail.com>
2026-04-09 10:45:37 -0400

    more fixes

--- templates/baked/src/lib/stories/geo-story-duckdb/data/metadata.csv.sh
+++ templates/baked/src/lib/stories/geo-story-duckdb/data/metadata.csv.sh
@@ -3,7 +3,7 @@
 # Download data files for geo-story-1
 # Run from this directory: ./metadata.csv.sh
 
-BASE_URL="https://raw.githubusercontent.com/jstonge/rdag-montreal/refs/heads/main/pipelines/transform/input/metadata.csv"
+BASE_URL="https://raw.githubusercontent.com/jstonge/rdag-montreal/refs/heads/main/pipelines/transform/input"
 
 curl -o metadata.csv "$BASE_URL/metadata.csv"
 
